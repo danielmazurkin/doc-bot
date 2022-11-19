e77c372155bcd98ad2cb7cf5399b69c7439c5923
Danil Mazurkin <mazurkin.daniel@gmail.com>
2022-11-19 13:33:58 +1000

    Добавил центровку по изображению и доделал формирование кода для юзера

--- doc_bot/common/use_case_image.py
+++ doc_bot/common/use_case_image.py
@@ -1,12 +1,24 @@
 from core.use_case import BaseUseCase
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
+from django.conf import settings
 
 
 class UseCaseImage(BaseUseCase):
     """ЮзКейс для формирования картинки с кодом."""
 
-    def execute(self, text):
-        img = Image.new('RGB', (512, 512), color='white')
-        d = ImageDraw.Draw(img)
-        d.text((150, 150), text, fill=(255, 255, 0))
-        img.save("pil_red.png")
+    def __create_image(self, size, bg_color, message, font, font_color) -> Image:
+        """Функция для создания изображений (центровка кода)."""
+        W, H = size
+        image = Image.new('RGB', size, bg_color)
+        draw = ImageDraw.Draw(image)
+        _, _, w, h = draw.textbbox((0, 0), message, font=font)
+        draw.text(((W - w) / 2, (H - h) / 2), message, font=font, fill=font_color)
+        return image
+
+    def execute(self, code_for_file) -> Image:
+        size = (300, 300)
+        font = ImageFont.truetype(f"{settings.STATIC_DIR}/fonts/Roboto-Bold.ttf", 45)
+        color_text = (35, 81, 247)
+        img = self.__create_image(size, (255, 255, 255), code_for_file, font, color_text)
+        img.save("code_for_user.png")
+        return img
